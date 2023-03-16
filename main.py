a0115a869b3036c7a94e027b67f0e61d87123881
khanhvipdinhpro <khanhvip972011@gmail.com>
2023-03-16 10:03:36 +0700

    add csrf = CSRFProtect(app) and csrf_token to all html files which have form

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm, CreateNe
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 app.app_context().push()
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+# Enable ckediter, bootstrap and csrf
 ckeditor = CKEditor(app)
 Bootstrap(app)
+csrf = CSRFProtect(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
